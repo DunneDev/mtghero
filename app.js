d9559345b00e5849685103c815558de0ef0116cf
DunneDev <dunn0203@algonquinlive.com>
2022-10-07 07:31:55 -0400

    show functionality

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const Card = require('./models/card');
-const { captureRejectionSymbol } = require('events');
-const { createDiffieHellman } = require('crypto');
 
 mongoose.connect('mongodb://localhost:27017/mtghero', {
     useNewUrlParser: true,
@@ -18,6 +17,7 @@ db.once('open', () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('path', path.join(__dirname, 'views'));
 
@@ -25,17 +25,27 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/test', async (req, res) => {
-    const card = new Card({
-        id: '323db259-d35e-467d-9a46-4adcb2fc107c',
-        name: 'Opt',
-        price: '0.10',
-        quantity: 5
-    });
-
-    await card.save();
-    res.send(card);
+app.get('/singles', async (req, res) => {
+    const cards = await Card.find({});
+    res.render('singles/index', { cards });
 });
+
+app.get('/singles/:id', async (req, res) => {
+    const card = await Card.findById(req.params.id);
+    res.render('singles/show', { card });
+});
+
+// app.get('/test', async (req, res) => {
+//     const card = new Card({
+//         id: '323db259-d35e-467d-9a46-4adcb2fc107c',
+//         name: 'Opt',
+//         price: '0.10',
+//         quantity: 5
+//     });
+
+//     await card.save();
+//     res.send(card);
+// });
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
